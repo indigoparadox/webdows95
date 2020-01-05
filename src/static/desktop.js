0ca143181f7b85cd663a1b055f8a0473e1f884a1
indigoparadox <indigo.repo@interfinitydynamics.info>
2020-01-04 23:29:11 -0500

    We have a basic nested folder browser.

--- src/static/desktop.js
+++ src/static/desktop.js
@@ -1,26 +1,78 @@
 
-$(document).ready( function() {
-    var win_folder = windowOpenFolder( 'Foo', 'foo', 'icons-w95-16x16.png', 112, 144 );
-    desktopCreateIcon( "Test.txt", 'icons-w95-32x32.png', 800, 544, 10, 10, function() {
+function populateFolder( parentWinHandle ) {
+    var folderPath = $(parentWinHandle).data( 'folder-path' );
 
-    }, win_folder.find( '.container' ) );
+    $(parentWinHandle).children( '.icon' ).remove();
+    $(parentWinHandle).children( '.window-form' ).children( '.container' ).children( '.icon' ).remove();
 
-    var ico_foo = desktopCreateIcon( "Foo", 'icons-w95-32x32.png', 800, 544, 10, 10, function() {
+    $.get( '/ajax/folders/' + folderPath, function( data ) {
+        console.log( data );
+        for( var iconIter in data.children ) {
+            folderData = data.children[iconIter];
+
+            // TODO: Handle nesting in IDs.
+            var folderIconId = 'icon-' + iconIter.replace( / /g, '-' );
+            var folderWindowId = 'window-' + iconIter.replace( / /g, '-' );
+
+            var container = $(parentWinHandle).find( '.container' );
+            if( 'desktop' == data.type ) {
+                container = '#desktop';
+            }
+
+            createFolderIcon(
+                container, folderPath, iconIter, folderIconId, folderWindowId,
+                folderData.iconX, folderData.iconY );
+        }
+    } );
+}
+
+function createFolderIcon( container, parentPath, name, folderIconID, folderWinID, x, y ) {
+
+    console.assert( $(container).length == 1 );
+
+    var icon = desktopCreateIcon( name, 'icons-w95-32x32.png', 256, 288, x, y, function() {
         /* Only open the window if it's not already open. */
+        if( 0 >= $('#' + folderWinID).length ) {
+            var winFolder = windowOpenFolder( name, folderWinID, 'icons-w95-16x16.png', 112, 144 );
+            winFolder.data( 'folder-path', parentPath + '/' + name );
+        }
+        windowActivate( '#desktop', '#' + folderWinID );
+        populateFolder( '#' + folderWinID );
+    }, container );
+
+    console.log( icon );
+
+    return icon;
+}
+
+$(document).ready( function() {
+    //var win_folder = windowOpenFolder( 'Foo', 'foo', 'icons-w95-16x16.png', 112, 144 );
+    /*desktopCreateIcon( "Test.txt", 'icons-w95-32x32.png', 800, 544, 10, 10, function() {
+
+    }, win_folder.find( '.container' ) );*/
+
+    /*
+    var ico_foo = desktopCreateIcon( "Foo", 'icons-w95-32x32.png', 800, 544, 10, 10, function() {
+        // Only open the window if it's not already open.
         if( 0 >= $('#window-foo').length ) {
             var win_foo = windowOpen( 'Foo', 'window-foo', true, 'icons-w95-16x16.png', 400, 272 );
             var input_txt = windowCreateInputText( win_foo, 'Date', '', '10px', '10px' );
         }
     } );
+    */
+    
+   populateFolder( '#desktop' );
 
-    var win_cmd = windowOpenCommand( 'Command', 'cmd', 'icons-w95-16x16.png', 128, 256 );
+    //var win_cmd = windowOpenCommand( 'Command', 'cmd', 'icons-w95-16x16.png', 128, 256 );
 
+    /*
     var win_props = windowOpenProperties( 'Properties', 'props' );
     var tab_general = windowPropertiesAddTab( win_props, "General" );
     tab_general.append( '<p>This is the general tab.</p>', 'tab-general' );
     var tab_other = windowPropertiesAddTab( win_props, "Other", 'tab-other' );
     tab_other.append( '<p>This is the other tab.</p>' );
     win_props.find( '.window-properties-tabs' ).tabs();
+    */
 
     $('#desktop').mousedown( function( e ) {
         if( $(e.target).hasClass( 'container' ) ) {
--- src/windows95/routes.py
+++ src/windows95/routes.py
@@ -1,6 +1,44 @@
 
-from flask import current_app, render_template
+from flask import current_app, render_template, jsonify
 import logging
+
+icons = {
+    'children': {
+        'desktop': {
+            'type': 'desktop',
+            'children': {
+                'A Folder': {
+                    'type': 'folder',
+                    'iconX': 50,
+                    'iconY': 50,
+                    'children': {
+                        'A Text File': {
+                            'type': 'notepad',
+                            'iconX': 10,
+                            'iconY': 10,
+                            'contents': 'This is a text file.'
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+@current_app.route( '/ajax/folders/<path:folder_path>' )
+def windows95_ajax_folders( folder_path ):
+    folder_path = folder_path.split( '/' )
+    folder_path.reverse()
+
+    folder_iter = ''
+    icons_iter = icons
+    while folder_path:
+        folder_iter = folder_path.pop()
+        print( folder_iter )
+        if 'children' in icons_iter and folder_iter in icons_iter['children']:
+            icons_iter = icons_iter['children'][folder_iter]
+
+    return jsonify( icons_iter );
 
 @current_app.route( '/' )
 def windows95_root():
