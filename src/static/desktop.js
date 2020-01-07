d2d8abcd9b5538412bf2766e6af43dfccb4c332f
indigoparadox <indigo.repo@interfinitydynamics.info>
2020-01-07 01:06:26 -0500

    Added to welcome message.

--- src/static/desktop.js
+++ src/static/desktop.js
@@ -29,7 +29,17 @@ var desktop = {"children":{
         "contents":`
 Welcome to the world of the past!
 
-This demonstration has a number of unique features in an attempt to simulate the feel of a bygone era.
+This demonstration has a number of unique features in an attempt to simulate the feel of a bygone era:
+
+ * The command prompt has an old-timey blinkey cursor and text feel.
+
+ * The web browser filters sends URL requests to the past (~1999) via archive.org.
+
+ * The CD player plays CD-quality music.
+
+ * Notepad!
+
+ Please feel free to look around and explore. Thank you for visiting!
 `,
         "iconX":20,
         "iconY":260,
