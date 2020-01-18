d1497b8532faa87bd43c59cf6f804a4bb498b38b
indigoparadox <indigo.repo@interfinitydynamics.info>
2020-01-17 21:38:33 -0500

    Pulled icon rendering out into system.

--- explorer.js
+++ explorer.js
@@ -5,12 +5,6 @@ $.fn.explorer95 = function( action, options ) {
 var settings = $.extend( {
     'caption': 'Explorer',
     'id': null,
-    'icoImg': null,
-    'icoX': 0,
-    'icoY': 0,
-    'icoW': 32,
-    'icoH': 32,
-    'menu': null,
     'x': 10,
     'y': 10,
     'w': 320,
@@ -20,44 +14,6 @@ var settings = $.extend( {
 }, options );
 
 switch( action.toLowerCase() ) {
-case 'icon':
-
-    var imgTag = $('<div class="desktop-icon-img icon-' + settings.icon + '-32"></div>');
-    var iconWrapper = $('<div class="desktop-icon"><div class="desktop-icon-overlay"></div>');
-    iconWrapper.append( imgTag );
-    
-    var iconText = $('<div class="desktop-icon-text-center"><div class="desktop-icon-text">' + settings.caption + '</div></div>');
-    iconWrapper.append( iconText );
-
-    this.append( iconWrapper );
-    iconWrapper.draggable( {'handle': '.desktop-icon-overlay', 'containment': this } );
-
-    iconWrapper.css( 'left', settings.x.toString() + 'px' );
-    iconWrapper.css( 'top', settings.y.toString() + 'px' );
-
-    /* Setup action handlers. */
-    iconWrapper.mousedown( function() {
-        $(this).explorer95( 'select' );
-    } );
-    iconWrapper.on( 'dblclick', settings.cbData, settings.callback );
-
-    return iconWrapper;
-
-case 'select':
-
-    return this.each( function( idx, element ) {
-        if( $(element).hasClass( 'desktop-icon' ) ) {
-            // A specific icon was provided. Deselect all peer icons.
-            $(element).parent().children('.desktop-icon').removeClass( 'desktop-icon-selected' );
-
-            // Select this icon.
-            $(element).addClass( 'desktop-icon-selected' );
-        } else {
-            // A container was provided. Deselect all icons inside.
-            $(element).children('.desktop-icon').removeClass( 'desktop-icon-selected' );
-        }    
-    } );
-
 
 case 'open':
 
