c4a6e47c234c82eea2dccc49d98ab8b802b9d21c
indigoparadox <indigo.repo@interfinitydynamics.info>
2020-01-20 07:35:01 -0500

    Roughly sketched out more consistent context menu handling.

--- explorer.js
+++ explorer.js
@@ -4,6 +4,7 @@ $.fn.explorer95 = function( action, options ) {
 
 var settings = $.extend( {
     'caption': 'Explorer',
+    'target': null,
     'id': null,
     'x': 10,
     'y': 10,
@@ -64,6 +65,55 @@ case 'open':
 
     console.assert( 1 == winHandle.length );
     console.assert( winHandle.hasClass( 'window-hidden' ) );
+
+    if( null != settings.target ) {
+        winHandle.addClass( 'explore-' + _htmlStrToClass( settings.target ) );
+    }
+
+    var folderMenu = {
+        'items': [
+            {'caption': 'Arrange Icons', 'type': menu95Type.SUBMENU, 'items': [
+                {'caption': 'By Name', 'callback': function( m ) {
+                    container.trigger( 'arrange-icons', [{'criteria': 'name'}] );
+                }},
+                {'caption': 'By Type', 'callback': function( m ) {
+                    container.trigger( 'arrange-icons', [{'criteria': 'type'}] );
+                }},
+                {'caption': 'By Size', 'callback': function( m ) {
+                    container.trigger( 'arrange-icons', [{'criteria': 'size'}] );
+                }},
+                {'caption': 'By Date', 'callback': function( m ) {
+                    container.trigger( 'arrange-icons', [{'criteria': 'date'}] );
+                }},
+                {'type': menu95Type.DIVIDER},
+                {'caption': 'Auto Arrange', 'callback': function( m ) {
+                    container.trigger( 'arrange-icons', [{'criteria': 'auto'}] );
+                }}
+            ]},
+            {'caption': 'Line up Icons', 'callback': function( m ) {
+                container.trigger( 'line-up-icons' );
+            }},
+            {'type': menu95Type.DIVIDER},
+            {'caption': 'Paste', 'callback': function( m ) {
+                container.trigger( 'paste', [{'reference': 'shortcut'}] );
+            }},
+            {'caption': 'Paste Shortcut', 'callback': function( m ) {
+                container.trigger( 'paste', [{'reference': 'shortcut'}] );
+            }},
+            {'type': menu95Type.DIVIDER},
+            {'caption': 'New', 'type': menu95Type.SUBMENU, 'items': [
+                {'caption': 'Folder', 'icon': 'folder', 'callback': function( m ) {
+                    container.trigger( 'new-folder' );
+                }}
+            ]},
+            {'type': menu95Type.DIVIDER},
+            {'caption': 'Properties', 'callback': function( m ) {
+                container.props95( props95Panel.DISPLAY );
+            }}
+        ]
+    };
+
+    container.menu95( 'context', {'menu': folderMenu, 'context': _htmlStrToClass( settings.target )} );
 
     winHandle.removeClass( 'window-hidden' );
 
